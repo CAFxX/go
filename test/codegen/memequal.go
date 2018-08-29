65963cb36ed20cfcba0fd7175c0ba08fdec6ed2d
Carlo Alberto Ferraris <cafxx@strayorange.com>
2018-08-29 23:08:58 +0900

    wip
    Change-Id: Idc023455f6ef7fd909aea25df3a7b09d1c95dce6

--- test/codegen/memequal.go
+++ test/codegen/memequal.go
@@ -11,7 +11,12 @@ package codegen
 type BoxType1 [1]byte
 type BoxType2 [2]byte
 type BoxType4 [4]byte
+type BoxType7 [7]byte
 type BoxType8 [8]byte
+type BoxType9 [9]byte
+type BoxType15 [15]byte
+type BoxType16 [16]byte
+type BoxType17 [17]byte
 
 func (t BoxType1) memeqsmall(s string) bool {
 	// 386:-".*memequal"
@@ -37,9 +42,50 @@ func (t BoxType4) memeqsmall(s string) bool {
 	return string(t[:]) == s
 }
 
-func (t BoxType8) memeqsmall(s string) bool {
+func (t BoxType7) memeqsmall(s string) bool {
+	// 386:-".*memequal"
 	// amd64:-".*memequal"
 	// arm64:-".*memequal"
 	// s390x:-".*memequal"
+	return string(t[:]) == s
+}
+
+func (t BoxType8) memeqsmall(s string) bool {
+	// 386:-".*memequal"
+	// amd64:-".*memequal"
+	// arm64:-".*memequal"
+	// s390x:-".*memequal"
+	return string(t[:]) == s
+}
+
+func (t BoxType9) memeqsmall(s string) bool {
+	// 386:".*memequal"
+	// amd64:-".*memequal"
+	// arm64:-".*memequal"
+	// s390x:-".*memequal"
+	return string(t[:]) == s
+}
+
+func (t BoxType15) memeqsmall(s string) bool {
+	// 386:".*memequal"
+	// amd64:-".*memequal"
+	// arm64:-".*memequal"
+	// s390x:-".*memequal"
+	return string(t[:]) == s
+}
+
+func (t BoxType16) memeqsmall(s string) bool {
+	// 386:".*memequal"
+	// amd64:-".*memequal"
+	// arm64:-".*memequal"
+	// s390x:-".*memequal"
+	return string(t[:]) == s
+}
+
+func (t BoxType17) memeqsmall(s string) bool {
+	// 386:".*memequal"
+	// amd64:".*memequal"
+	// arm64:".*memequal"
+	// s390x:".*memequal"
 	return string(t[:]) == s
 }
