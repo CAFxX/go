2c7c69ebbbc6949f1f9f37d9cfdb3140d8964517
Carlo Alberto Ferraris <cafxx@mercari.com>
2021-09-22 19:37:33 +0900

    fix sp

--- src/runtime/decommit_unix.go
+++ src/runtime/decommit_unix.go
@@ -31,7 +31,7 @@ func decommitUnusedStack() {
 	// decommit is nosplit, so it can use at most _StackLimit bytes,
 	// so we assume that _StackLimit past the current sp will be used
 	// by decommit itself, and we ask to decommit the remaining space.
-	decommit(gp.stack.lo, gp.sched.sp-_StackLimit)
+	decommit(gp.stack.lo, getcallersp()-_StackLimit)
 	procUnpin()
 }
 
@@ -51,7 +51,7 @@ func decommit(start, end uintptr) {
 	if start >= end {
 		return
 	}
-	if GOOS == "linux" || GOOS == "darwin" || GOOS == "aix" {
+	if GOOS == "linux" || GOOS == "aix" {
 		madvise(unsafe.Pointer(start), end-start, _MADV_DONTNEED)
 	} else {
 		madvise(unsafe.Pointer(start), end-start, _MADV_FREE)
