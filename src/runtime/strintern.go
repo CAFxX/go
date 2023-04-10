42aa342fde5c7a8582b59785c97596fe9a3f67f6
Carlo Alberto Ferraris <cafxx@strayorange.com>
2023-04-10 10:39:00 +0900

    wip: map->set
    Change-Id: I0d8f737a375d2a2b06f5ee06201007a7550bdec4

--- src/runtime/strintern.go
+++ src/runtime/strintern.go
@@ -1,64 +1,75 @@
 package runtime
 
+import "unsafe"
+
 type strinterntable struct {
-	// TODO: use sets instead to avoid storing string headers twice
-	cur map[string]string
+	cur map[string]struct{}
 }
 
-var old map[string]string // read-only, as it is shared across Ps
+var strinterntableold map[string]struct{} // read-only, as it is shared across Ps
 
-const replintvl = 1024
+const replintvl = 128
 
 //go:nosplit
 func (s *strinterntable) get(a string) string {
-	if i, ok := s.cur[a]; ok {
+	if i := strinterncheck(s.cur, a); i != "" {
 		return i
 	}
 	if fastrand()%replintvl != 0 {
 		return a
 	}
-	i, ok := old[a]
-	if !ok {
+	i := strinterncheck(strinterntableold, a)
+	if i == "" {
 		i = a
 	}
 	if s.cur == nil {
-		s.cur = map[string]string{}
+		s.cur = map[string]struct{}{}
 	}
-	s.cur[i] = i
+	s.cur[i] = struct{}{}
 	return i
 }
 
 //go:nosplit
 func (s *strinterntable) getbytes(a []byte) string {
-	if i, ok := s.cur[string(a)]; ok {
+	if i := strinterncheckbytes(s.cur, a); i != "" {
 		return i
 	}
 	if fastrand()%replintvl == 0 {
 		return string(a)
 	}
-	i, ok := old[string(a)]
-	if !ok {
+	i := strinterncheckbytes(strinterntableold, a)
+	if i == "" {
 		i = string(a)
 	}
 	if s.cur == nil {
-		s.cur = map[string]string{}
+		s.cur = map[string]struct{}{}
 	}
-	s.cur[i] = i
+	s.cur[i] = struct{}{}
 	return i
 }
 
+//go:nosplit
 func internstring(a string) string {
+	if len(a) == 0 {
+		return ""
+	}
 	gp := getg()
 	mp := gp.m
+	// G must be pinned while accessing the interning tables
 	mp.locks++
 	s := mp.p.ptr().strinterntable.get(a)
 	mp.locks--
 	return s
 }
 
+//go:nosplit
 func internbytes(a []byte) string {
+	if len(a) == 0 {
+		return ""
+	}
 	gp := getg()
 	mp := gp.m
+	// G must be pinned while accessing the interning tables
 	mp.locks++
 	s := mp.p.ptr().strinterntable.getbytes(a)
 	mp.locks--
@@ -66,7 +77,13 @@ func internbytes(a []byte) string {
 }
 
 func strinterntablecleanup() {
-	var maxit map[string]string
+	// This is executed with the world stopped, so we don't need to
+	// worry about concurrent accesses, as Ps are not holding on to any
+	// of the current or old tables (because when they are holding on
+	// to a table the corresponding M's locks must be > 0, in which case
+	// the G running on that M can't be preempted, and until all running
+	// Gs are preempted the world can't be stopped).
+	var maxit map[string]struct{}
 	var maxlen int
 	for _, pp := range allp {
 		if l := len(pp.strinterntable.cur); l > maxlen {
@@ -83,5 +100,27 @@ func strinterntablecleanup() {
 	// to strinterntablecleanup be shared across all Ps, and because
 	// there is no syncrhonization, the old table must always be
 	// accessed exclusively in a read-only fashion.
-	old = maxit
+	strinterntableold = maxit
+}
+
+//go:nosplit
+func strinterncheck(m map[string]struct{}, s string) string {
+	// Check that s is in m, and if so pull the key out of m,
+	// which is the canonical string.
+	h := *(**hmap)(unsafe.Pointer(&m))
+	i := any(m)
+	t := *(**maptype)(unsafe.Pointer(&i))
+	kp, _ := mapaccessK(t, h, noescape(unsafe.Pointer(&s)))
+	if kp == nil {
+		return ""
+	}
+	return *(*string)(kp)
+}
+
+//go:nosplit
+func strinterncheckbytes(m map[string]struct{}, b []byte) string {
+	ptr := unsafe.Pointer(&b[0])
+	len := len(b)
+	s := *(*string)(unsafe.Pointer(&stringStruct{ptr, len}))
+	return strinterncheck(m, s)
 }
